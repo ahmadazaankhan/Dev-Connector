d317f833ec324afd1872cf141b46f34b38d765a9
Ahmad Azaan <ahmadazaankhan@gmail.com>
2026-06-30 19:15:25 +0500

    Redux

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import './App.css';
 
 import { Provider } from 'react-redux';
+import store from './store';
 
 // const App = () => (
 //   <Router>
@@ -24,18 +25,20 @@ import { Provider } from 'react-redux';
 // );
 
 const App = () => (
+  <Provider store = {store}>
   <Router>
     <Fragment>
       <Navbar />
+      <Route exact path='/' element={<Landing />} />
       <section className='container'>
         <Routes>
-          <Route exact path='/' element={<Landing />} />
           <Route exact path='/register' element={<Register />} />
           <Route exact path='/login' element={<Login />} />
         </Routes>
       </section>
     </Fragment>
   </Router>
+  </Provider>
 );
 
 export default App;
